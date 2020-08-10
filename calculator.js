e78fa332aa2fa0765d62471dee3d8cc3c656f29a
SaulHernandez230 <SaulHernandez@230.github.com>
2020-08-10 14:53:57 -0600

    finish calculator

--- calculator.js
+++ calculator.js
@@ -2,15 +2,32 @@
     var input = document.querySelector("input")
     var item = document.querySelectorAll('div button')
     var string  = ""
+    var a = 0 , b = 0
+    var operator = ""
     item.forEach(link => {
         link.addEventListener('click',(e)=>{
             // link.style.background = "red"
             let character = link.textContent
             switch (character) {
                 case 'C':
+                    string = input.value
                     string = string.slice(0,-1)
                 break
+
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    operator = character
+                    a = +string
+                    string = ""
+                break
                 case "=":
+                    b = +string
+                   string = operator === "+" ? a + b : 
+                            operator === "-" ? a - b :
+                            operator === "*" ? a * b :
+                            operator === "/" ? a / b : "error"
                 break;
                 default:
                     string += character
